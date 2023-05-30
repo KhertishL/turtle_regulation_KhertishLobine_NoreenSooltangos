0e8ca41b322d6fd7866a7fc648ed5e7d66059336
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 <khertishl@gmail.com>
2023-05-30 14:39:59 +0400

    commit #3

--- src/set_way_point.py
+++ src/set_way_point.py
@@ -4,7 +4,7 @@ import math
 from std_msgs.msg import String
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-from math import atan2, atan, sqrt
+from math import atan2, atan
 
 global coords
 coords = [7,7]
@@ -18,44 +18,62 @@ def Subscriber_pose():
 	rate = rospy.Rate(10)
 
 	kp = rospy.get_param("~kp", 1.0)
+	kpl = rospy.get_param("~kpl", 1.0)
 
 	while not rospy.is_shutdown():
-		message = Twist()
-
-
 
 		if turtlePose is not None:
-			theta = angle()
-			print(theta)
+			rotate(kp, pub, rate)
+			is_moving(pub, rate, kpl)
 
-			error_angle = error(theta)
-			#print("Error Angle = ", error_angle)
-			u = kp * error_angle
-			message.angular.z = u
 			print(message)
-
-
-		pub.publish(message)
-		rate.sleep()
 
 def getPosition(pose):
 	global turtlePose
  	turtlePose = pose
 
+
+
+def rotate(kp, pub,rate):
+	message = Twist()
+	theta = angle()
+	print(theta)
+
+	error_angle = error(theta)
+	#print("Error Angle = ", error_angle)
+	u = kp * error_angle
+	message.angular.z = u
+
+	pub.publish(message)
+	rate.sleep()
+
+
+def is_moving(pub ,rate ,kpl):
+	message = Twist()
+
+	y_length = (coords[1] - turtlePose.y)* (coords[1] - turtlePose.y)
+	x_length = (coords[0] - turtlePose.x)* (coords[0] - turtlePose.x)
+
+
+	e = math.sqrt(y_length + x_length)
+
+	v = kpl * e
+	message.linear.x = v
+	print("Commande Lineaire = ", v)
+
+	distance_travelled = 0
+	while(distance_travelled < e):
+		pub.publish(message)
+		rate.sleep()
+
+
+
 def angle():
 	return atan2(coords[1] - turtlePose.y ,coords[0] - turtlePose.x)
-
-def angular_vel(kp, error_angle):
-	return kp * error_angle
-
 
 def error(theta):
 
 	return atan(math.tan((theta - turtlePose.theta)/2))
-
-#def distance()
-	#return sqrt((coords[1]-turtlePose.x)^2 + (coords[0]-turtlePose)^2)
-
 
 
 if __name__=="__main__":
